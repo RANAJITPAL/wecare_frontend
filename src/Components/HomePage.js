c74fbe5cc9c8614676219a1ae58aa5d37ce807ed
Ranajit Pal <palranajit98@gmail.com>
2021-05-11 11:03:40 +0530

    add footer

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,33 +1,44 @@
 
-import { AppBar, Card, CardContent, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Card, CardContent, Toolbar, Typography, Box } from '@material-ui/core';
 import './HomePage.css';
 function HomePage() {
     return (
         <div>
             <div>
-            <AppBar position='static'>
-                <Toolbar>
-                    <Typography variant="h6">
-                        WeCare
+                <AppBar position='static'>
+                    <Toolbar>
+                        <Typography variant="h6">
+                            WeCare
                     </Typography>
-                </Toolbar>
-            </AppBar>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <div style={{ padding: '35px' }}>
+                <Box display="flex" flexDirection="row" justifyContent="center" >
+                    <div style={{ padding: '25px' }}>
+                        <Card variant='elevation' className='card_style' m={10} p={10}>
+                            <CardContent>
+                                <text>card 1</text>
+                            </CardContent>
+                        </Card>
+                    </div>
+                    <div style={{ padding: '25px' }}>
+                        <Card variant='elevation' className='card_style'>
+                            <CardContent>
+                                <text>card 2</text>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </Box>
             </div>
             <div>
-                <div>
-                <Card variant = 'elevation' className = 'card_style'>
-                    <CardContent>
-                        <text>card 1</text>
-                    </CardContent>
-                </Card>
-                </div>
-                <div>
-                <Card variant = 'elevation' className = 'card_style'>
-                    <CardContent>
-                        <text>card 2</text>
-                    </CardContent>
-                </Card>
-                </div>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <Typography variant="h6">
+                            footer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
             </div>
         </div>
     );
